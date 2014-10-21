e9761e304b809eb43b325a700c12209b84c3f04f
Caleb <hoffmc3@rpi.edu>
2014-10-21 16:40:11 -0400

    Basic movement

---
+++ Robot/Assets/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+
+	Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)){ //0 is left, 1 is right
+			tf.Translate(0.1f, 0f, 0f);
+		}
+		if(Input.GetKey(KeyCode.A)){ //0 is left, 1 is right
+			tf.Translate(0f, 0f, 0.1f);
+		}
+		if(Input.GetKey(KeyCode.S)){ //0 is left, 1 is right
+			tf.Translate(-0.1f, 0f, 0f);
+		}
+		if(Input.GetKey(KeyCode.D)){ //0 is left, 1 is right
+			tf.Translate(0f, 0f, -0.1f);
+		}
+	}
+}
