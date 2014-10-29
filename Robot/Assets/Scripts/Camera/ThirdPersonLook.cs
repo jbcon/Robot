aaed438d033ad9497cc957074b6eebc999d18619
Greg <gregweil@pentavera.com>
2014-10-29 16:01:02 -0400

    Added the ability to look up and down

--- Robot/Assets/Scripts/Camera/ThirdPersonLook.cs
+++ Robot/Assets/Scripts/Camera/ThirdPersonLook.cs
@@ -6,14 +6,22 @@ public class ThirdPersonLook : MonoBehaviour {
 	float sensitivity = 5.0f;
 	Transform tf;
 
+	float rotationY;
+
+	float rotYMin = -75;
+	float rotYMax = 85;
+
 	// Use this for initialization
 	void Start () {
 		tf = GetComponent<Transform> ();
+		rotationY = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//tf.Rotate (tf.right, (Input.GetAxis ("Mouse Y") * sensitivity));
-		tf.Rotate (Vector3.up, (Input.GetAxis ("Mouse X") * sensitivity));
+		Vector2 rot = (new Vector2 (Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y")) * sensitivity);
+		float rotationX = (tf.localEulerAngles.y + rot.x);
+		rotationY = Mathf.Clamp ((rotationY + rot.y), rotYMin, rotYMax);
+		tf.localEulerAngles = new Vector3 (-rotationY, rotationX, 0f);
 	}
 }
