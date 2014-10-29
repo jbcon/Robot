2d06ded9c7efa5edcb698b77a6684915007431d8
Caleb <hoffmc3@rpi.edu>
2014-10-29 14:39:11 -0400

    Fixed camera position, added script for pickups

---
+++ Robot/Assets/CollectObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnControllerColliderHit (ControllerColliderHit col)
+	{
+		Debug.Log ("hit");
+		if(col.gameObject.name == "Collectable")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
