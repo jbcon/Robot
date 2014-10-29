b338f52fb7c9e0312191b947c1eb31ae7c7b34c9
John Conover <111jbcon@gmail.com>
2014-10-29 11:52:01 -0400

    Added mountains, water

---
+++ Robot/Assets/Scripts/GlobalState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalState : MonoBehaviour {
+
+
+	public static int gameState;
+	/* Global state of game world
+	 * 0: no collectibles
+	 * 1: one collectible collected
+	 * 2: two	"			"
+	 * 3: three	"			"		, exit opens
+	 */
+
+	// Update is called once per frame
+	void start() {
+		gameState = 0;
+	}
+
+	void Update () {
+	
+	}
+}
