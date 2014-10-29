f36bc8fa39bcc89c1140654127b4b2cde1f31477
Greg <gregweil@pentavera.com>
2014-10-29 17:25:14 -0400

    Initial player attack implementation

---
+++ Robot/Assets/Scripts/Player/PlayerAttackEnemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttackEnemy : MonoBehaviour {
+
+	Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Ray ray = new Ray (tf.position, tf.forward);
+			RaycastHit hit;
+			if (Physics.SphereCast (ray, 1f, out hit, 2.5f)) {
+				if (hit.collider.gameObject.tag == "Enemy") {
+					Debug.Log (hit.point);
+				}
+			}
+		}
+	}
+}
