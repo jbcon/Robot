8cbba009dcbcd33ad29d8dfa5d377232710032f1
Caleb <hoffmc3@rpi.edu>
2014-11-02 14:05:11 -0500

    gears face camera

---
+++ Robot/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 point = Camera.main.transform.position;
+		point.y = 0;
+		tf.LookAt (point);
+	}
+}
