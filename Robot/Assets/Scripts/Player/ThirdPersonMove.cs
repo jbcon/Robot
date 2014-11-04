143ef414d160e5e073d5c2b7206b9fb6f46bbbdf
Greg <gregweil@pentavera.com>
2014-11-04 17:26:01 -0500

    Basic movement animations

--- Robot/Assets/Scripts/Player/ThirdPersonMove.cs
+++ Robot/Assets/Scripts/Player/ThirdPersonMove.cs
@@ -6,6 +6,8 @@ public class ThirdPersonMove : MonoBehaviour {
 	Transform tf;
 	Transform cam;
 
+	Animator anim;
+
 	Vector3 push = Vector3.zero;
 	float pushProp = 0f;
 
@@ -13,6 +15,7 @@ public class ThirdPersonMove : MonoBehaviour {
 	void Start () {
 		tf = GetComponent<Transform> ();
 		cam = GetComponentInChildren<ThirdPersonLook> ().GetComponent<Transform> ();
+		anim = GetComponentInChildren<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +25,7 @@ public class ThirdPersonMove : MonoBehaviour {
 		move = (cam.rotation * move);
 		move.y = 0.0f;
 		move = (move.normalized * moveSpeed);
+		anim.SetBool ("Walking", (move.magnitude > 0.25f));
 		//Rotate to match movement
 		if (move != Vector3.zero) {
 			Quaternion rot = cam.rotation;
@@ -36,6 +40,7 @@ public class ThirdPersonMove : MonoBehaviour {
 		//Send the move command
 		SendMessage ("SetInputMoveDirection", move);
 		SendMessage ("SetInputJump", Input.GetButton ("Jump"));
+		if (Input.GetButtonDown ("Jump")) anim.SetTrigger ("Jump");
 	}
 
 	public void Shove (Vector3 from) {
