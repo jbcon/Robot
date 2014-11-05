309da757f3e96dac84a29980e282076748250d5a
Greg Weil <gregweil@pentavera.com>
2014-11-04 23:00:07 -0500

    Added animation sound effect calls

---
+++ Robot/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	AudioSource source;	
+
+	// Use this for initialization
+	void Start () {
+		source = gameObject.AddComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlaySoundOnce (AudioClip sound) {
+		source.PlayOneShot (sound);
+	}
+}
