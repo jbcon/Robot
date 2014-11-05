542643fa9c2dfd68644ad8da663be8a02253210f
Greg Weil <gregweil@pentavera.com>
2014-11-04 23:15:21 -0500

    Added random wind sounds

---
+++ Robot/Assets/Scripts/WindSounds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindSounds : MonoBehaviour {
+
+	AudioSource source;
+	float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		if (time < 0f) {
+			source.PlayOneShot (source.clip);
+			time += Random.Range (1f, 2.5f);
+		}
+	}
+}
