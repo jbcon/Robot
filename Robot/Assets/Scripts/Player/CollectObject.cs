93ee3fae72303bda29e6e16574f3d5c3cd0504b8
Caleb <hoffmc3@rpi.edu>
2014-11-04 22:17:28 -0500

    Ability to end the game

--- Robot/Assets/Scripts/Player/CollectObject.cs
+++ Robot/Assets/Scripts/Player/CollectObject.cs
@@ -15,9 +15,9 @@ public class CollectObject : MonoBehaviour {
 		image1 = Resources.Load ("Slide1") as Texture;
 		image2 = Resources.Load ("Slide2") as Texture;
 		image3 = Resources.Load ("Slide3") as Texture;
-		image4 = Resources.Load ("Slide1") as Texture;
-		image5 = Resources.Load ("Slide2") as Texture;
-		image6 = Resources.Load ("Slide3") as Texture;
+		image4 = Resources.Load ("Slide4") as Texture;
+		image5 = Resources.Load ("Slide5") as Texture;
+		image6 = Resources.Load ("Slide6") as Texture;
 		image7 = Resources.Load ("Slide1") as Texture;
 		image8 = Resources.Load ("Slide2") as Texture;
 		image9 = Resources.Load ("Slide3") as Texture;
